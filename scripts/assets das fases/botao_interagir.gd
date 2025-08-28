e73eb63e039b605d8b8fae97a2fbc7760801633f
sloomy <andreyribeiror2005@gmail.com>
2025-08-28 19:53:46 -0300

    Tileset feito e aplicado em 3 dos 5 níveis.

--- scripts/assets das fases/botao_interagir.gd
+++ scripts/assets das fases/botao_interagir.gd
@@ -3,14 +3,18 @@ extends Node2D
 @onready var temporizador_botoes = $Area2D/Timer
 @export var plataformas: Array[Node2D]
 
+@onready var sprite = $Area2D/sprite2d
+
 func _process(delta: float) -> void:
 	for nodes in plataformas:
 		if temporizador_botoes.is_stopped():
 			nodes.get_node("AnimatableBody2D/CollisionShape2D").disabled = true
 			nodes.visible = false
+			sprite.animation = "off"
 		else:
 			nodes.get_node("AnimatableBody2D/CollisionShape2D").disabled = false
 			nodes.visible = true
+			sprite.animation = "on"
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
--- scripts/assets das fases/plataforma_farinha.gd
+++ scripts/assets das fases/plataforma_farinha.gd
@@ -3,6 +3,13 @@ extends Node2D
 @onready var timer = $Timer
 @onready var cooldown = $cooldown
 @onready var colisao = $StaticBody2D/CollisionShape2D
+@onready var anim = $StaticBody2D/AnimatedSprite2D
+
+func _process(delta: float) -> void:
+	if timer.is_stopped():
+		anim.pause()
+	else:
+		anim.play("break")
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
